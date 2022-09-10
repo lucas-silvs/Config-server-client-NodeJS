b8e8ddf79bb53bc30a6cc5ed2c23288b54ef731f
Lucas Santos <lucas.parkl@outlook.com>
2022-09-10 16:31:42 -0300

    adicionando basic auth para pegar configurações do config-server

--- utilizando lib cloud-config-client/node-with-config-client.js
+++ utilizando lib cloud-config-client/node-with-config-client.js
@@ -1,21 +1,34 @@
 const client = require("cloud-config-client");
+
+// uma das formar de carrega os dados do config-server
+
+
+var nomeServico;
 client.load({
     name: "service--teste", //nome da aplicação
     profiles:['dev'], // spring profiles 
-    endpoint: "http://localhost:8888"
+    endpoint: "http://localhost:8888", //URI do Config-Server
+    auth: {user: "developer", pass: "dummypassword"},
+    context: process.env
 }).then((config) => {
+    configLocal = config
     // Look for a key
-    const value1 = config.get("service.name");
-    console.log("const value1 = config.get(\"service.name\");: " + String(value1))
+    nomeServico = config.get("service.name");
+    console.log("const value1 = config.get(\"service.name\");: " + String(nomeServico))
 
     // Using a prefix, this is equivalent to .get("this.is.another.key");
     const value2 = config.get("service", "version")
     console.log("const value2 = config.get(\"service\", \"version\");: " + String(value2))
-
 });
+
 
 // Or
 
-// const config = await client.load({ application: 'service--teste' })
+// const config =  client.load({
+//     name: "service--teste", //nome da aplicação
+//     profiles:['dev'], // spring profiles 
+//     endpoint: "http://localhost:8888", //URI do Config-Server
+//     auth: {user: "developer", pass: "dummypassword"}
+// })
 // const value2 = config.get("service.name");
-// console.log("teste outro tipo: " + value2)n
+// console.log("teste outro tipo: " + String(value2))
