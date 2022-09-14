065040421a6024eab344addebf50b4ac991259b5
Lucas Santos <lucas.parkl@outlook.com>
2022-09-14 09:30:18 -0300

    Adicionando projeto exemplo NodeJs com config Server

--- utilizando lib cloud-config-client/client-utilizando-dotenv.js
+++ utilizando lib cloud-config-client/client-utilizando-dotenv.js
@@ -2,12 +2,21 @@
 //os campos devem ser em não deve ser utilizado '.' para  
 require("dotenv").config()
 
-// o javascript enxerga as propriedades do arquivo .env como variaveis de ambiente
-console.log(process.env.SERVER_PORT)
-console.log(process.env.CAMPO_TESTE)
+var express = require ('express'); 
+var app = express();
+app.get('/', function (req, res) {
+    //captura campo chamado CAMPO_TESTE que foi recebido do config--server e convertido para o .env
+    res.send(process.env.CAMPO_TESTE);
+});
+app.listen(3000, function() {
+    // o javascript enxerga as propriedades do arquivo .env como variaveis de ambiente
+console.log("Server iniciado e escutando na porta "+process.env.SERVER_PORT)
 
 //campos estilo yaml podem ser capturados com o exemplo abaixo
 console.log(process.env["spring.datasource.url"])
-
 console.log(process.env["spring.datasource.username"])
+});
+
+
+
 
